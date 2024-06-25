339aaf4b35ccf2a3d99540d83054c488bf356108
RunSabba <abbasnur24@gmail.com>
2024-06-25 08:35:41 -0400

    Adding an output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 #think of output as a print statement
 
-output "hello-world" {
+/*output "hello-world" {
   description = "Print a Hello World text output"
   value       = "Hello World"
 }
@@ -14,7 +14,7 @@ output "public_url" {
   description = "Public URL for our Web Server"
   value       = "https://${aws_instance.web_server.public_ip}:8080/index.html"
 }
-
+*/
 output "vpc_information" {
   description = "VPC Information about Environment"
   value       = "Your ${aws_vpc.vpc.tags.Environment} VPC has an ID of ${aws_vpc.vpc.id}"
