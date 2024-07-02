4ad2540803b1de88967caee0dbd7aa173ee2cf2e
RunSabba <abbasnur24@gmail.com>
2024-07-02 11:11:20 -0400

    added a module into my main.tf to create multiple servers

--- main.tf
+++ main.tf
@@ -293,3 +293,20 @@ resource "aws_security_group" "vpc-ping" {
 
 }
 
+module "server" {
+  source    = "./server"
+  ami       = data.aws_ami.ubuntu_free_tier.id
+  subnet_id = aws_subnet.public_subnets["public_subnet_3"].id
+  security_groups = [aws_security_group.ingress-ssh.id,
+    aws_security_group.my-new-security-group.id,
+    aws_security_group.vpc-ping.id,
+  aws_security_group.vpc-web.id]
+}  
+
+output "public_ip" {
+  value = module.server.public_ip
+}
+
+output "public_dns" {
+  value = module.server.public_dns
+}
