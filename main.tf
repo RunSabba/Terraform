8fe6bce5a4b6a250da9407bc17b119c1f40dc686
RunSabba <abbasnur24@gmail.com>
2024-07-03 11:16:41 -0400

    added addtional module for a web server

--- main.tf
+++ main.tf
@@ -294,21 +294,25 @@ resource "aws_security_group" "vpc-ping" {
 }
 
 module "server" {
-  source    = "./server"
+  source    = "./modules/server"
   ami       = data.aws_ami.ubuntu_free_tier.id
   subnet_id = aws_subnet.public_subnets["public_subnet_3"].id
-  security_groups = [aws_security_group.ingress-ssh.id,
-    aws_security_group.my-new-security-group.id,
+  security_groups = [
     aws_security_group.vpc-ping.id,
-    aws_security_group.vpc-web.id]
+    aws_security_group.ingress-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
 }
 
-module "server_sub1" {
-  source    = "./server"
-  ami       = data.aws_ami.ubuntu_free_tier.id
-  subnet_id = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups = [aws_security_group.ingress-ssh.id,
-    aws_security_group.my-new-security-group.id,
-    aws_security_group.vpc-ping.id,
-    aws_security_group.vpc-web.id]
+module "server_subnet_1" {
+  source          = "./modules/web_server"
+  ami             = data.aws_ami.ubuntu_free_tier.id
+  key_name        = aws_key_pair.generated.key_name
+  user            = "ubuntu"
+  private_key     = tls_private_key.generated.private_key_pem
+  subnet_id       = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id, 
+  aws_security_group.ingress-ssh.id, 
+  aws_security_group.vpc-web.id]
 }
+
